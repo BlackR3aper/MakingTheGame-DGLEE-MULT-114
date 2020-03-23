d43ff0b9d29a4761968477a6966d6cbbe6c97815
BlackR3aper <60709462+BlackR3aper@users.noreply.github.com>
2020-03-23 11:36:11 -0700

    MakingTheGame DGLEE MULT 114 Update 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{//Private means: No reason to access the script in Unity
+    private Rigidbody PlayerRigidbody;
+    public float forceMultiplier;
+    public float gravityMultiplier;
+    public bool onGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround )
+        { PlayerRigidbody.AddForce(Vector3.up * forceMultiplier, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+
+    }
+}
