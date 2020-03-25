e436b87abeabe2e5b65648d1961e7507803141df
BlackR3aper <60709462+BlackR3aper@users.noreply.github.com>
2020-03-25 13:22:34 -0700

    Lesson 3B

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject bgObstaclePrefab;
     private Vector3 spawnLocation = new Vector3(30, 0, 0);
     private float initialWait = 2;
     private float periodRepeat = 2;
+    private PlayerController playerController;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", 2,2);
+        InvokeRepeating("SpawnObstacle", initialWait, periodRepeat);
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        int bgBoxCount = 20;
+        float bgWidth = GameObject.Find("Background").GetComponent<BoxCollider>().size.x;
+        float spacing = bgWidth / bgBoxCount;
+
+
+        for (int count = 0; count < bgBoxCount; count++)
+        {
+            Debug.Log(count * spacing);
+            Instantiate(bgObstaclePrefab, new Vector3(count * spacing, 0, 4.15f), bgObstaclePrefab.transform.rotation);
+        }
     }
 
     // Update is called once per frame
@@ -24,9 +38,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
-            
+        // I accidently  put captial 'P' instead of 'p'
+        if (playerController.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+        }
     }
-
-
 }
